950dbe247cb8f35cf3d44f8fec376737fa4fe1b7
Shin Sungkyu <106169271+ssk1109@users.noreply.github.com>
2025-03-23 14:56:53 +0000

    done: db연결(db.js) working: app.js

--- my_blog/src/config/db.js
+++ my_blog/src/config/db.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose');
 
-// 난 바꾼게 있는데 왜 안되지
+require('dotenv').config(); // .env에 있는 db uri 가져오기
+
+// mongoDB 연결하는 '함수' ->  재사용성, 코드 유지보수성 up
+const connectDB = async () => { // db연결시간이 길어질 수도 있어 비동기로 실행
+    try{
+        await mongoose.connect(process.env.DB_URI);
+        // mongoose.connect(uri주소);
+        // 'process.env.환경변수명'으로 .env에 있는 원하는 환경변수 사용가능
+        console.log('몽고연결!')
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+// 다른 파일에서 사용하기 위해 exports
+module.exports = connectDB;
