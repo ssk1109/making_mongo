4b9714ca0d893079bfff685c6db82e187e6ed4b0
Shin Sungkyu <106169271+ssk1109@users.noreply.github.com>
2025-03-24 08:58:12 +0000

    middleware 제외한 app.js 설정 완료!

--- my_blog/src/app.js
+++ my_blog/src/app.js
@@ -5,3 +5,11 @@ const app = express();
 require('dotenv').config();
 
 connectDB();
+
+// routes안에 있는 라우트 설정을 불러옴
+const authRoutes = require('./routes/auth.js');
+const postRoutes = require('./routes/post');
+
+// 실제 HTTP요청이 들어왔을 때 사용할 routes와 연결
+app.use('/api/auth', authRoutes);
+app.use('/api.pose', postRoutes);
