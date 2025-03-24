58ff3716c868b59d8b70b99abcd7e80535898f21
Shin Sungkyu <106169271+ssk1109@users.noreply.github.com>
2025-03-24 11:07:10 +0000

    routes가 아닌 models완 이었습니다

--- my_blog/src/routes/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    content: {type: String, required: true},
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, // User모델의 objectId를 userId로 그대로 사용하겠다
-});
-
-module.exports = mongoose.model('Post', postSchema);
